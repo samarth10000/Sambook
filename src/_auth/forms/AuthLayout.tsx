ec1367f197f204328054110eaf9e387ef0e1c144
Samarth Saxena <163576924+samarth10000@users.noreply.github.com>
2025-05-24 21:55:10 +0530

    some changes

--- src/_auth/forms/AuthLayout.tsx
+++ src/_auth/forms/AuthLayout.tsx
@@ -13,7 +13,7 @@ const AuthLayout = () => {
             <Outlet />
           </section>
           <img
-            src="/assets/images/side-img.svg"
+            src="/assets/images/6310507.jpg"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
